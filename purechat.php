d8dfbe3fad7f56d113d3c2eaea5861e1a760cbda
pure-chat <pure-chat@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-02 16:58:09 +0000

    Version 1.32 (tag)
    git-svn-id: https://plugins.svn.wordpress.org/pure-chat/tags/1.32@1126467 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ purechat.php
@@ -0,0 +1,157 @@
+<?php
+/**
+ * @package Pure_Chat
+ * @version 1.32
+ */
+/*
+Plugin Name: Pure Chat
+Plugin URI:
+Description: Website chat, simplified. Now from right inside WordPress!
+Author: Pure Chat, Inc.
+Version: 1.32
+Author URI: purechat.com
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+include 'variables.php';
+
+class Pure_Chat_Plugin {
+	var $version = 5;
+
+	public static function activate()	{
+		Pure_Chat_Plugin::clear_cache();
+	}
+
+	public static function deactivate()	{
+		Pure_Chat_Plugin::clear_cache();
+	}
+
+	function Pure_Chat_Plugin() {
+		add_option('purechat_widget_code', '', '', 'yes');
+		add_option('purechat_widget_name', '', '', 'yes');
+
+		add_action('admin_menu', array( &$this, 'pure_chat_menu' ) );
+		add_action('wp_ajax_pure_chat_update', array( &$this, 'pure_chat_update' ) );
+
+		if ( get_option('purechat_widget_code') != '') {
+			add_action('wp_enqueue_scripts', array( &$this, 'load_pure_chat_scripts' ) );
+		}
+
+		$this->update_plugin();
+	}
+
+	function update_plugin() {
+		if ( get_option('purechat_plugin_ver', 0) < $this->version && get_option('purechat_widget_code') != '') {
+			$this->pure_chat_update_script();
+		}
+
+		update_option('purechat_plugin_ver', $this->version);
+	}
+
+	function pure_chat_menu() {
+		add_menu_page('Pure Chat', 'Pure Chat', 'manage_options', 'purechat-menu', array( &$this, 'pure_chat_generateAcctPage' ), plugins_url().'/pure-chat/favicon.ico');
+	}
+
+	function pure_chat_update() {
+		update_option('purechat_widget_code', $_POST['purechatwid']);
+		update_option('purechat_widget_name', $_POST['purechatwname']);
+		$this->pure_chat_update_script();
+		add_action('wp_enqueue_scripts', array( &$this, 'load_pure_chat_scripts' ) );
+	}
+
+	function pure_chat_update_script() {
+		$w = get_option('purechat_widget_code');
+		$file = plugin_dir_path(__FILE__)."/purechatwidgetcode.js";
+		$wjs = "(function () { var done = false; var script = document.createElement('script'); script.async = true; script.type = 'text/javascript'; script.src = 'https://app.purechat.com/VisitorWidget/WidgetScript'; document.getElementsByTagName('HEAD').item(0).appendChild(script); script.onreadystatechange = script.onload = function (e) { if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) { var w = new PCWidget({pureServerUrl: 'https://app.purechat.com', c: '{$w}', f: true }); done = true; } }; })();";
+		file_put_contents($file, $wjs);
+		Pure_Chat_Plugin::clear_cache();
+	}
+
+	private static function clear_cache() {
+		if (function_exists('wp_cache_clear_cache')) {
+			wp_cache_clear_cache();
+		}
+	}
+
+	function pure_chat_generateAcctPage() {
+		global $purechatHome;
+		?>
+		<head>
+				<link rel="stylesheet" href="<?php echo plugins_url().'/pure-chat/purechatStyles.css'?>" type="text/css">
+		</head>
+		<?php
+		if (isset($_POST['purechatwid']) && isset($_POST['purechatwname'])) {
+			pure_chat_update();
+		}
+		?>
+		<p>
+		<div class="purechatbuttonbox">
+			<img src="<?php echo plugins_url().'/pure-chat/logo.png'?>"alt="Pure Chat logo"></img>
+			<div class = "purechatcontentdiv">
+				<?php
+				if (get_option('purechat_widget_code') == '' ) {
+					?>
+					<p>Pure Chat allows you to chat in real time with visitors to your WordPress site. Click the button below to get started by logging in to Pure Chat and selecting a chat widget!</p>
+					<p>The button will open a widget selector in an external page. Keep in mind that your Pure Chat account is separate from your WordPress account.</p>
+				<?php
+				} else {
+				?>
+					<h4>Your current chat widget is:</h4>
+					<h1 class="purechatCurrentWidgetName"><?php echo get_option('purechat_widget_name'); ?></h1>
+					<p>Would you like to switch widgets?</p>
+				<?php
+				}
+				?>
+			</div>
+			<form>
+				<input type="button" class="purechatbutton" value="Pick a widget!" onclick="openPureChatChildWindow()">
+			</form>
+			<p>
+		</div>
+		<script>
+			var pureChatChildWindow;
+			var purechatNameToPass = "<?php echo get_option('purechat_widget_name');?>";
+			var purechatIdToPass = "<?php echo get_option('purechat_widget_code');?>";
+			function openPureChatChildWindow() {
+				pureChatChildWindow = window.open('<?php echo $purechatHome;?>/home/pagechoicewordpress?widForDisplay=' + purechatIdToPass +
+											  '&nameForDisplay=' + purechatNameToPass, 'Pure Chat');
+			}
+			var url = ajaxurl;
+			window.addEventListener('message', function(event) {
+				var data = {
+					'action': 'pure_chat_update',
+					'purechatwid': event.data.id,
+					'purechatwname': event.data.name
+				};
+				jQuery.post(url, data).done(function(){})
+				var purechatNamePassedIn = event.data.name;
+				if(typeof purechatNamePassedIn != 'undefined') {
+					document.getElementsByClassName('purechatcontentdiv')[0].innerHTML = '<h4>Your current chat widget is:</h4><h1 class="purechatCurrentWidgetName">' +
+																						  purechatNamePassedIn + '</h1><p>Would you like to switch widgets?</p>';
+					purechatNameToPass = purechatNamePassedIn;
+					purechatIdToPass = event.data.id;
+				}
+			}, false);
+		</script>
+		<div class="purechatlinkbox">
+			<p><a href="https://app.purechat.com/user/dashboard" target="_blank">Your Pure Chat dashboard page</a> is your place to answer chats, add more widgets, customize their appearance with images and text, manage users, and more!</p>
+		</div>
+		<?php
+	}
+
+  function load_pure_chat_scripts() {
+		global $pagenow;
+		if (!is_admin() && 'wp-login.php' != $pagenow) {
+			wp_deregister_script( 'w_script' );
+			wp_register_script('w_script', plugins_url('purechatwidgetcode.js', __FILE__), array(), 1, true);
+			wp_enqueue_script('w_script');
+  	}
+  }
+
+}
+
+register_activation_hook(__FILE__, array('Pure_Chat_Plugin', 'activate'));
+register_deactivation_hook(__FILE__, array('Pure_Chat_Plugin', 'deactivate'));
+
+new Pure_Chat_Plugin();
---
+++ readme.txt
@@ -0,0 +1,113 @@
+=== Pure Chat - Free Live Chat Plugin ===
+Contributors: pure-chat
+Donate link: https://www.purechat.com
+Tags: chat, livechat, live, live chat, purechat, pure chat, online chat, chat online, wordpress chat, customer support, live help, chat widget, customer help, chat widget
+Requires at least: 3.0.1
+Tested up to: 4.1.1
+Stable tag: trunk
+
+Pure Chat is a leading live chat software that allows businesses to engage their customers in real time, from anywhere. Sign up and try it for free!
+
+== Description ==
+
+Pure Chat is a web app that makes it easy for website owners to chat with their visitors from anywhere! When users create an account and activate our simple, lightweight
+Pure Chat plugin for WordPress, they'll be able to design fully-customized chat widgets that automatically appear on their site. Other awesome features include:
+
+* Fully customizable chat widget (including language customization)
+
+* Respond to visitors on the go with the Pure Chat app for iOS and Android
+
+* Geolocation data for users -- know where your visitors are from!
+
+* Ban users by IP
+
+* Daily emails that breakdown chat activity (optional)
+
+* Full chat transcript history
+
+* User ratings for interactions
+
+* Visitors can leave contact info when you're not online
+
+* Customizable canned responses for common questions
+
+* Free accounts for one user, and just $5 for each additional teammate
+
+== Installation ==
+
+1. Install and activate plugin.
+2. Click on the new Pure Chat button in your left sidebar.
+3. Click the button to take you to the Pure Chat widget selector.
+4. Sign in or create a new account (If you're not already signed in to Pure Chat).
+5. Select a widget for your page.
+6. Click the link to your Pure Chat dashboard and get ready to chat!
+
+== Frequently Asked Questions ==
+
+= Do I have to install any extra software? =
+
+No, just this plugin.
+
+= How can I change the appearance of the widget on my page? =
+
+You can change all your widget settings from your [Pure Chat dashboard](https://app.purechat.com/user/dashboard/ "Pure Chat dashboard").
+
+= What pricing plans do you offer? =
+
+Check the [Pure Chat pricing page](https://www.purechat.com/pricing "Pure Chat pricing page") for the latest info! We offer both free and paid plans.
+
+= Does Pure Chat support other languages? =
+
+Absolutely! You can customize all the messages that appear in the chat widget and write them in whatever language you want.
+
+= Are there any ads? =
+
+No! There are no annoying ads in Pure Chat.
+
+= What do my visitors see if I'm not online to take their chats? =
+
+You decide! Your widget can display an offline indicator, disappear entirely, or let visitors email you with their questions.
+
+= Can I temporarily remove the chat widget from my site without losing my settings? =
+
+Yes, deactivating the Pure Chat plugin will stop the widget from showing up, but your preferred widget settings will remain for when you want to reactivate it.
+
+== Screenshots ==
+
+1. Chat widget
+
+2. Operator dashboard
+
+3. Chat with customers on the go with our native iOS (iPhone/iPad) and Android apps.
+
+== Changelog ==
+
+= 1.32 =
+* Potential fix for a rare bug with some users.
+
+= 1.31 =
+* Fixed uninstall cleanup.
+
+= 1.3 =
+* Fixed some more issues with WP Super Cache plugin.
+* Added proper cleanup on plugin uninstall.
+
+= 1.2 =
+* Fixed selected widget not updating right away due to recent "WP Super Cache" plugin update.
+
+= 1.1 =
+* Fixed bugs in 1.0
+
+= 1.0 =
+* Plugin launch.
+
+== Upgrade Notice ==
+
+= 1.2 =
+* Fixed selected widget not updating right away due to recent "WP Super Cache" plugin update.
+
+= 1.1 =
+* Fixed bugs in 1.0
+
+= 1.0 =
+* Plugin launch.
